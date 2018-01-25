845ae500e64134a54413688401b49231227e19cb
Kazuki Taira <hagegalka@gmail.com>
2018-01-25 14:14:49 +0900

    arr追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,36 @@
 	<b-container fluid class="top text-center d-flex flex-column justify-content-end">
 		<b-row class="border border-info mb-auto text-center d-block"><h1>header</h1></b-row>
 		<b-row class="border border-success h-100">
-			<b-col sm="2" class="border border-info">side</b-col>
-			<b-col class="border border-danger">main</b-col>
+			<b-col sm="2" class="border border-info">
+				<b-row class="h-100" style="overflow:scroll;">
+					<b-col class="d-flex justify-content-between flex-column-reverse">
+						<img src="../assets/logo.png"/>
+						<ul>
+							<li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+							<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+							<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+							<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+						</ul>
+						<h6>Ecosystem</h6>
+						<ul>
+							<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+							<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+							<li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+							<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+							<br>
+							<li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+						</ul>
+						<h6>Essential Links</h6>
+						<b-btn @click="onClick">押して</b-btn>
+						<!--h3>{{ msg }}</h3-->
+					</b-col>
+				</b-row>
+			</b-col>
+			<b-col class="border border-danger d-flex flex-column-reverse h-100" style="height:100%;overflow-y:scroll;">
+				<b-row v-for="(v, id) in arr" :key="id">
+					<b-col><h3 class="border border-info d-block">{{v}}</h3></b-col>
+				</b-row>
+			</b-col>
 		</b-row>
 		<b-row class="border border-warining text-center d-block"><h1>footer</h1></b-row>
 		<!--b-card class="h-100 w-100 p-0 m-0 d-inline-block">
@@ -37,7 +65,9 @@
 						<h2>Essential Links</h2>
 						<h1>{{ msg }}</h1>
 					</b-col>
-					<b-col>fuga</b-col>
+					<b-col>
+					<b-alert v-repeat="v in arr">{{v}}</b-alert>
+					</b-col>
 				</b-row>
 				<b-row style="margin-bottom:0;">bar</b-row>
 			</b-card>
@@ -82,12 +112,16 @@ export default {
   data () {
     return {
 		msg: 'Hello World',
+		arr: [],
 		dismissSecs: 10,
 		dismissCountDown: 0,
 		showDismissibleAlert: false
     }
   },
   methods: {
+	  onClick(e){
+		  this.arr.push(new Date());
+	  },
     countDownChanged (dismissCountDown) {
       this.dismissCountDown = dismissCountDown
     },
